239255d04f630177949d10c4747c0bb6da51a449
tlaudahl <travis.laudahl@gmail.com>
2019-11-11 11:31:16 -0600

    HASH

--- api/api-router.js
+++ api/api-router.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
 
 const authRouter = require('../auth/auth-router.js');
 const usersRouter = require('../users/users-router.js');
@@ -9,5 +10,19 @@ router.use('/users', usersRouter);
 router.get('/', (req, res) => {
   res.json({ api: "It's alive" });
 });
+
+router.post('/hash', (req, res) => {
+  // read a password from the body
+  // hash a password
+  // return in to the user in an object that looks like
+  // { password: 'original password', hash: 'hashed password' }
+  const password = req.body.password;
+  bcrypt.genSalt(password, salt, (err, hash) => {
+    res.status(200).json({
+      password: password,
+      hash: hash
+    })
+  })
+})
 
 module.exports = router;
